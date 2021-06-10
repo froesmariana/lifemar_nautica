2bd516073e35d318ea77fdf8114d5e85529cce4b
Mariana Froes <froes.mariana@gmail.com>
2021-06-10 17:19:21 -0300

    ajsutes do login.php -  blog site

--- admin/login.php
+++ admin/login.php
@@ -4,14 +4,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale-1,shrink-to-fit=no">
-        <base href="<?php ur">
+        <base href="<?php echo url_site;?>">
 
         <!--css-->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="../css/style.css">
         <link rel="stylesheet" href="dist/ui/trumbowyg.min.css">
 
-        <title> ADMIN CP</title>
+        <title><?php echo titulo_site;?></title>
     </head>
     <body>
         <br>
